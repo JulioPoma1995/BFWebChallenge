2c119405fa5bf83a5285a22184b495d6d5c4ef6f
Julio <juliopomap.j@gmail.com>
2024-02-09 02:09:06 -0500

    configuracion de jenkins

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -3,9 +3,9 @@ package runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-import org.junit.AfterClass;
+
 import org.junit.runner.RunWith;
-import pages.BasePage;
+
 
 
 @RunWith(Cucumber.class)
